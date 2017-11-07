e4eb85152a58483dc0b701c5a011d07596a3eb33
LancerComet <chw644@hotmail.com>
2017-11-07 16:18:03 +0800

    + Add complex querying support.

--- test/index.js
+++ test/index.js
@@ -3,4 +3,5 @@
 Function.prototype.bind = require('function-bind')
 
 // require all test files (files that ends with .spec.js)
+require('./specs/format-params.spec.js')
 require('./specs/vue-jsonp.spec.js')
